71fef0accff3dbd6ab465bfd71af8133f71f2d1f
Marcel-MSC <marcelomarcos.s.c@gmail.com>
2024-05-18 07:08:28 -0300

    Changed env and variable on contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,17 +33,17 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.EMAILJS_SERVICE_ID,
+        import.meta.env.EMAILJS_TEMPLATE_ID,
         1,
         {
           from_name: form.name,
           to_name: "JavaScript Mastery",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "marcelomarcos.s.c@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.EMAILJS_PUBLIC
       )
       .then(
         () => {
